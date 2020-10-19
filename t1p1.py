147617dd22cbb927677a64c1bf61aee484a7cbab
Felipe Vergara M <felip.vergara.m@gmail.com>
2020-10-18 21:11:26 -0300

    Agregado el segundo filtro de la primera parte

--- t1p1.py
+++ t1p1.py
@@ -4,7 +4,7 @@ Created on Sun Oct 18 10:58:12 2020
 
 @author: Juan Vergara
 """
-import scipy
+import scipy.io
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,13 +23,30 @@ filtro_norm = filtro/np.linalg.norm(filtro)
 #plt.imshow(filtro)
 conv = signal.convolve2d(img,filtro_norm)
 
-fig=plt.figure()
-fig.add_subplot(1,3,1)
+fig1=plt.figure()
+fig1.add_subplot(1,3,1)
 plt.imshow(img)
-fig.add_subplot(1,3,2)
+fig1.add_subplot(1,3,2)
 plt.imshow(filtro_norm)
-fig.add_subplot(1,3,3)
+fig1.add_subplot(1,3,3)
 plt.imshow(conv)
+
+min_val = np.min(filtro_norm)
+max_val = np.max(filtro_norm)
+fact = 1+2*min_val/(max_val-min_val)
+filtro_2 = -filtro_norm*fact + max_val*fact - min_val
+
+conv2 = signal.convolve2d(img,filtro_2)
+
+fig2=plt.figure()
+fig2.add_subplot(1,3,1)
+plt.imshow(img)
+fig2.add_subplot(1,3,2)
+plt.imshow(filtro_2)
+fig2.add_subplot(1,3,3)
+plt.imshow(conv2)
+
+plt.show()
 #fig, ax = plt.subplots(1,2)
 #ax[0,0].imshow(img)
 #ax[0,1].imshow(filtro)
