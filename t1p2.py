88af07bd328341774cef6d2749fc1d4a3ad067b4
Felipe Vergara M <felip.vergara@gmail.com>
2020-10-19 22:25:43 -0300

    listos todos los filtros

--- t1p2.py
+++ t1p2.py
@@ -21,43 +21,39 @@ plt.imshow(img)
 fig.add_subplot(1,2,2)
 plt.imshow(conv)
 
-diff = 1
-for i in range(30):
-    conv2 = np.zeros(img.shape)
-    d2=i+1+diff
-    filtro2 = np.ones((d2,d2))/(d2**2)
-    for channel in range(3):
-        conv2[:,:,channel] = signal.convolve2d(img[:,:,channel], filtro2,mode='same')
-    conv2=np.uint8(conv2)
 
-    conv3 = np.zeros(img.shape)
-    d3=i+1
-    filtro3 = np.ones((d3,d3))/(d3**2)
-    for channel in range(3):
-        conv3[:,:,channel] = signal.convolve2d(img[:,:,channel], filtro3,mode='same')
-    conv3=np.uint8(conv3)
 
-        #lowpass = ndimage.gaussian_filter(img, 10)
+conv2 = np.zeros(img.shape)
+d2=15
+filtro2 = np.ones((d2,d2))/(d2**2)
+for channel in range(3):
+    conv2[:,:,channel] = signal.convolve2d(img[:,:,channel], filtro2,mode='same')
+conv2=np.uint8(conv2)
 
-        #gauss_highpass = img - lowpasd
-        #f1=3
-        #f2=10
-        #gauss_bandpass = ndimage.gaussian_filter(img, f1) - ndimage.gaussian_filter(img, f2)
+conv3 = np.zeros(img.shape)
+d3=13
+filtro3 = np.ones((d3,d3))/(d3**2)
+for channel in range(3):
+    conv3[:,:,channel] = signal.convolve2d(img[:,:,channel], filtro3,mode='same')
+conv3=np.uint8(conv3)
 
-    fig2=plt.figure()
-    fig2.add_subplot(1,4,1)
-    plt.imshow(img)
-    fig2.add_subplot(1,4,2)
-    plt.imshow(conv2)
-    plt.title('d2 = %d' %(d2))
-    #plt.imshow(ndimage.gaussian_filter(img, f1))
-    fig2.add_subplot(1,4,3)
-    plt.imshow(conv3)
-    plt.title('d3 = %d' %(d3))
-    #plt.imshow(ndimage.gaussian_filter(img, f2))
-    fig2.add_subplot(1,4,4)
-    plt.imshow((conv3-conv2))
-    #plt.imshow(gauss_bandpass)
-    plt.savefig('test/%d_%d'%(diff,d3))
-    plt.close(fig2)
+conv4= (conv3-conv2)//4
+
+
+fig2=plt.figure()
+fig2.add_subplot(1,4,1)
+plt.imshow(img)
+fig2.add_subplot(1,4,2)
+plt.imshow(conv2)
+plt.title('d2 = %d' %(d2))
+#plt.imshow(ndimage.gaussian_filter(img, f1))
+fig2.add_subplot(1,4,3)
+plt.imshow(conv3)
+plt.title('d3 = %d' %(d3))
+#plt.imshow(ndimage.gaussian_filter(img, f2))
+fig2.add_subplot(1,4,4)
+plt.imshow(conv4)
+#plt.imshow(gauss_bandpass)
+plt.savefig('test/00_2_13_3')
+plt.close(fig2)
 #plt.show()
